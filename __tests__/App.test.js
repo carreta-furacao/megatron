86f1162a159a6a509df40de1d11d5172f8ea0328
Eduardo Moroni <eduardomoroni@gmail.com>
2017-09-19 16:32:44 -0300

    ci: comentar tests que foram ignorados

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -3,7 +3,8 @@ import renderer from 'react-test-renderer'
 
 import App from '../src/main'
 
-it('renders without crashing', () => {
+fit('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON()
   expect(rendered).toBeTruthy()
+  expect(true).toBe(false);
 })
--- dangerfile.js
+++ dangerfile.js
@@ -2,11 +2,35 @@ import { danger, markdown } from 'danger'
 import _ from 'lodash'
 import fs from 'fs'
 
-const prepushLog = 'prepush.log'
-const linterOutput = fs.readFileSync(prepushLog).toString()
+checkLinterErrors()
+checkSkippedTests()
 
-if (_.contains(linterOutput, 'Failed')) {
-  markdown(`These changes failed to pass the linter:
-${linterOutput}
-  `)
+function checkSkippedTests() {
+  const modifiedSpecFiles =  danger.git.modified_files.filter(filePath => filePath.match(/.test.(js|jsx)$/gi))
+
+  const testFilesIncludeExclusion = modifiedSpecFiles.reduce((acc, value) => {
+    const content = fs.readFileSync(value).toString()
+    const invalid = _.includes(content, 'it.only') || _.includes(content, 'describe.only') || _.includes(content, 'fdescribe') || _.includes(content, 'fit(')
+    if (invalid) { acc.push(value) }
+    return acc
+  }, [])
+
+  if (testFilesIncludeExclusion.length > 0) {
+    fail(`skipped test was left in tests (${testFilesIncludeExclusion})`)
+  }
+}
+
+function checkLinterErrors() {
+  const linterOutput = fs.readFileSync('prepush.log').toString()
+  const thereWarningOrErrors = _.includes(linterOutput, 'error') || _.includes(linterOutput, 'warning')
+
+  if (thereWarningOrErrors) {
+    markdown('O linter está reclamando sobre: ')
+
+    _.each(linterOutput.split('\n'), (line) => {
+      if (!_.startsWith(line, '>')) {
+        markdown(line)
+      }
+    })
+  }
 }
