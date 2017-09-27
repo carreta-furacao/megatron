777781ed44885628dd43ec4816139bb3f18465d5
Conference <conference@techopss-macbook-pro.local.gap.com>
2017-09-27 11:25:27 -0300

    refactor(congressmenList): torna componente funcional puro

--- src/modules/congressmen/containers/congressmenList.js
+++ src/modules/congressmen/containers/congressmenList.js
@@ -1,22 +1,16 @@
+import React from 'react'
 import PropTypes from 'prop-types'
 import { FlatList } from 'react-native'
-import React, { Component } from 'react'
 import { Profile } from '../components/profile'
 
-export class CongressmenList extends Component {
-  static propTypes = {
-    congressmen: PropTypes.array.isRequired
-  }
+export const CongressmenList = props => (
+  <FlatList
+    data={props.congressmen}
+    keyExtractor={item => item.id}
+    renderItem={({ item }) => (<Profile congressman={item} />)}
+  />
+)
 
-  renderCongressman = ({ item }) => (<Profile congressman={item} />)
-
-  render() {
-    return (
-      <FlatList
-        data={this.props.congressmen}
-        keyExtractor={item => item.id}
-        renderItem={this.renderCongressman}
-      />
-    )
-  }
+CongressmenList.propTypes = {
+  congressmen: PropTypes.array.isRequired
 }
